39eb39ce8ae2656221e1d5f462aa892a53ce1fdc
Sooji Kim <yh0gogo@gmail.com>
2020-03-18 14:28:10 +0900

    Add specific presets options,  publicPath@output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   name: "react with webpack setting",
-  mode: "development",
-  devtool: "eval",
+  mode: "development", // production에서는 'production'
+  devtool: "eval", // production에서는 'hidden-source-map'
   resolve: {
     extensions: [".js", ".jsx"]
   },
@@ -18,14 +19,33 @@ module.exports = {
         test: /\.jsx?/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+          // babel 설정
+          presets: [
+            // plugin들의 모임
+            [
+              "@babel/preset-env",
+              {
+                // 특정 preset에 대한 설정
+                targets: {
+                  browsers: ["> 5% in KR"] // 브라우저 버전 대응
+                }
+                //debug: true
+              }
+            ],
+            "@babel/preset-react"
+          ],
+          plugins: ["react-hot-loader/babel"]
         }
       }
     ]
   },
+  plugins: [
+    // 확장 기능 : webpack을 통해 추가적으로 설정하고 싶은 내용들 기입
+  ],
 
   output: {
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist", // webpack-dev-server 사용 시에도 path를 dist로 지정(가상 경로 같음)
     filename: "bundle.js"
   }
 };
